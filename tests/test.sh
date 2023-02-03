e0514b57efb186a9f811c416096408e222788f00
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 15:55:15 +0300

    install git hook

---
+++ tests/test.sh
@@ -0,0 +1,9 @@
+#/!bin/bash
+
+log=/tmp/test.dump
+echo > $log
+
+rm -rf vendor
+rm -rf composer.lock
+composer install --dev
+/usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
