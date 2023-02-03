013799e098864ea325bdf0b726bef8803803cfae
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 21:03:02 +0300

    fixed tests status report

--- tests/test.sh
+++ tests/test.sh
@@ -7,5 +7,7 @@ rm -rf vendor
 rm -rf composer.lock
 composer install --dev
 rm -rf /tmp/GalleryBundle
+
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
-echo $?
+status=$(cat $log | grep "ERRORS!")
+[ -z "$status" ] && exit 0 ||  exit -1
