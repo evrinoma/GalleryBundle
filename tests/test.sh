8c3db19d9a42e1af3ebb3f0c071d40e5c7a626ec
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 18:19:10 +0300

    fixed showing wrong results

--- tests/test.sh
+++ tests/test.sh
@@ -7,3 +7,4 @@ rm -rf vendor
 rm -rf composer.lock
 composer install --dev
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
+echo $?
