c08649a14c7cae411b7400be24dee1f092b5125a
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:24:03 +0300

    fixed test clean up

--- tests/test.sh
+++ tests/test.sh
@@ -6,5 +6,6 @@ echo > $log
 rm -rf vendor
 rm -rf composer.lock
 composer install --dev
+rm -rf /tmp/GalleryBundle
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
 echo $?
